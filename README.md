baf297b7f76be6c79f9e4f558134029e89e30b36
Michael Linhardt <linhardt.michael@gmail.com>
2023-04-21 01:22:00 +0700

    first request to ChatGPT ->
    Write a NodeJS script, when running, it take inputs from the terminal. After each input the terminal display "loading..." and it disappear when the script finish to execute the command, given the listening the user input again.
    
    This script accept 2 commands in input : !script and !help
    
    When typing !help, it display the man of this script.
    
    When typing !script only, it show an error because it require an argument.
    
    When typing !script following by a string of character called instruction, the script will concatenate this message :
    
     "Write a NodeJS script with detailed commentary. Implement in the script a simple log output that write in a txt file in the same folder. The log file purpose is to track and understand what happen when the script is executed. Only output the script without anything else. I will now describe what should do the script :"
    
    with the instruction entered in the prompt from the command !script.
    
    Here is an example of usage :
    "!script write a script that read the column A from an excel file and add in column B the value A/2"
    
    After concatenate the 2 string, the script will use the ChatGPT API with a key and send the concatenated string to ChatGPT. The answer from ChatGPT is expected to be a script, it will be written in the file "silentWorker.js", erasing it's previous content first.
    
    The script will then execute silentWorker.js and give back the prompt to user when its done.
    
    When the script is running and executing the command typed by the user, it will output in the terminal, detailed log of the script progress.

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-# node_chatgpt_silent_worker
-Execute script written by chatGPT on the fly
 
-This script is written by Chat GPT and you can see it's evolution in the commit
+# NodeJS ChatGPT Terminal Interface
+
+This project provides a NodeJS terminal interface for interacting with OpenAI's ChatGPT.
+
+## Note from Mike
+
+"Everything is written by ChatGPT, even this README file.
+You can find in each commit description, the chat typed for the cahnges obtained."
+
+## Features
+
+- Get script generated from ChatGPT
+- Write and execute generated scripts
+- Re-execute previously generated scripts
+- Extensible command system
+
+## Installation
+
+1. Clone the repository:
+
+```git clone <https://github.com/michaellinhardt/node_chatgpt_silent_worker>
+cd node_chatgpt_silent_worker```
+
+2. Install the required dependencies:
+
+```npm install```
+
+Add your ChatGPT API key to script.js:
+
+```const CHATGPT_API_KEY = "your-chatgpt-api-key-here";```
+
+Run the script:
+
+```node script.js```
+
+## Usage
+
+Enter one of the following commands in the terminal:
+
+!help: Display the help text.
+!script: Concatenate the instruction with a predefined message and send it to ChatGPT. Requires an argument (instruction).
+!rerun: Re-execute the previously generated silentWorker.js file.
+
+For example:
+
+```node script.js !script reads column A from an excel file and add in column B the value A/2```
---
+++ script.js
@@ -0,0 +1,90 @@
+const fs = require("fs");
+const readline = require("readline");
+const { exec } = require("child_process");
+const axios = require("axios");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const CHATGPT_API_KEY = "your-chatgpt-api-key-here";
+
+const helpText = `
+Usage: node script.js [command] [argument]
+
+Commands:
+  !help         Display this help text.
+  !script       Concatenate the instruction with a predefined message and send it to ChatGPT.
+                Requires an argument (instruction).
+
+Examples:
+  !script write a script that reads column A from an excel file and add in column B the value A/2
+`;
+
+function sendToChatGPT(instruction) {
+  return axios.post(
+    "https://api.openai.com/v1/engines/davinci-codex/completions",
+    {
+      prompt: instruction,
+      max_tokens: 200,
+      n: 1,
+      stop: null,
+      temperature: 0.7,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${CHATGPT_API_KEY}`,
+      },
+    }
+  );
+}
+
+function processCommand(input) {
+  console.log("loading...");
+  const [command, ...args] = input.trim().split(" ");
+
+  if (command === "!help") {
+    console.log(helpText);
+    rl.prompt();
+  } else if (command === "!script") {
+    if (args.length === 0) {
+      console.error("Error: !script command requires an argument.");
+      rl.prompt();
+    } else {
+      const instruction = args.join(" ");
+      const message = `Write a NodeJS script with detailed commentary. Implement in the script a simple log output that write in a txt file in the same folder. The log file purpose is to track and understand what happen when the script is executed. Only output the script without anything else. I will now describe what should do the script: ${instruction}`;
+
+      sendToChatGPT(message)
+        .then((response) => {
+          const script = response.data.choices[0].text.trim();
+          fs.writeFileSync("silentWorker.js", script);
+          exec("node silentWorker.js", (error, stdout, stderr) => {
+            if (error) {
+              console.error(`Error executing silentWorker.js: ${error.message}`);
+            }
+            if (stderr) {
+              console.error(`Error in silentWorker.js: ${stderr}`);
+            }
+            console.log(stdout);
+            rl.prompt();
+          });
+        })
+        .catch((error) => {
+          console.error(`Error sending request to ChatGPT: ${error.message}`);
+          rl.prompt();
+        });
+    }
+  } else {
+    console.error("Error: Unrecognized command.");
+    rl.prompt();
+  }
+}
+
+rl.on("line", (input) => {
+  processCommand(input);
+});
+
+console.log("Welcome! Type !help for usage instructions.");
+rl.prompt();
