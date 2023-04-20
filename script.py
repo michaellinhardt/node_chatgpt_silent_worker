05122b7dc051f6ef11626003f8bcbc06721c3b27
Michael Linhardt <linhardt.michael@gmail.com>
2023-04-21 01:46:42 +0700

    re-write the script with commentary and ouput information about the script steps in the terminal

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ from getpass import getpass
 
 CHATGPT_API_KEY = "your-chatgpt-api-key-here"
 
+# Function to send a request to ChatGPT with the given prompt
 def chatgpt_request(prompt):
     url = "https://api.openai.com/v1/engines/davinci-codex/completions"
     headers = {"Authorization": f"Bearer {CHATGPT_API_KEY}"}
@@ -21,27 +22,37 @@ def chatgpt_request(prompt):
     return response.json()["choices"][0]["text"].strip()
 
 def main():
+    print("Welcome to the ChatGPT Terminal Interface!")
+    
     while True:
         user_input = input("> ")
-        
+
+        # Display help text
         if user_input == "!help":
             print("This script accepts the following commands:")
             print("!script - Generates a script with the provided instruction.")
             print("!help - Displays this help text.")
         
+        # Generate and execute a script based on the user's instruction
         elif user_input.startswith("!script"):
             instruction = user_input[len("!script"):].strip()
+            
+            # Check if an instruction is provided
             if not instruction:
                 print("Error: The !script command requires an argument.")
                 continue
             
+            print("Generating script...")
             prompt = f"Write a Python script with detailed commentary. Implement in the script a simple log output that writes in a txt file in the same folder. The log file purpose is to track and understand what happens when the script is executed. Only output the script without anything else. I will now describe what the script should do: {instruction}"
             generated_script = chatgpt_request(prompt)
             
+            print("Saving generated script as 'silentWorker.py'...")
             with open("silentWorker.py", "w") as f:
                 f.write(generated_script)
             
+            print("Executing 'silentWorker.py'...")
             os.system("python silentWorker.py")
+            print("Execution finished.")
         
         else:
             print("Invalid command. Type !help for available commands.")
